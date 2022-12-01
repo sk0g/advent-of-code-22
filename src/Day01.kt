5d874742f45080207c310e2cbfbb4622278cab9a
sk0g <ani.mehta@mail.com>
2022-12-01 17:37:51 +1000

    1-1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val calorieCounts: MutableList<Int> = mutableListOf()
+        var currentCalorieCount = 0
+
+        for (calorie in input) {
+            val parsedCalorie = calorie.toIntOrNull()
+
+            if (parsedCalorie == null) {
+                calorieCounts += currentCalorieCount
+                currentCalorieCount = 0
+            } else {
+                currentCalorieCount += parsedCalorie
+            }
+        }
+
+        return calorieCounts.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return 0
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
 }
