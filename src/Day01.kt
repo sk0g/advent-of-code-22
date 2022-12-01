402a0a0730898d44cf70baed13d591572fd56e7b
sk0g <ani.mehta@mail.com>
2022-12-01 17:45:31 +1000

    1-2

--- src/Day01.kt
+++ src/Day01.kt
@@ -18,9 +18,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val calorieCounts: MutableList<Int> = mutableListOf()
+        var currentCalorieCount = 0
+
+        for (calorie in input) {
+            val parsedCalorie = calorie.toIntOrNull()
+
+            if (parsedCalorie == null) {
+                calorieCounts += currentCalorieCount
+                currentCalorieCount = 0
+            } else {
+                currentCalorieCount += parsedCalorie
+            }
+        }
+
+        return calorieCounts
+            .sorted()
+            .drop(calorieCounts.size - 3)
+            .sum()
     }
 
     val input = readInput("Day01")
+
     println(part1(input))
+    println(part2(input))
 }
