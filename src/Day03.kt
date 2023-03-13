c79c9e3898cd3f960bdabf08a494743ed7d07b7d
sk0g <ani.mehta@mail.com>
2023-03-13 21:17:43 +1000

    3-2

--- src/Day03.kt
+++ src/Day03.kt
@@ -12,16 +12,59 @@ fun Char.getPriority(): Int =
         this.code + 1 - 'a'.code
     }
 
+fun getCommonItemAndPriority(items: List<String>): Pair<Char, Int> {
+    assert(items.size == 3)
+    val (s1, s2, s3) = items
+    val commonCharacter = s1.toHashSet()
+        .intersect(s2.toHashSet())
+        .intersect(s3.toHashSet())
+        .first()
+
+    return commonCharacter to commonCharacter.getPriority()
+}
+
 fun main() {
     val input = readInput("Day03")
 
+    part1Assertions()
+    val part1Value = input.sumOf { findCommonItem(it).getPriority() }
+    println("Part 1: $part1Value")
+
+    part2Assertions()
+    val part2Value = input
+        .chunked(3)
+        .map { getCommonItemAndPriority(it) }
+        .sumOf { it.second }
+    println("Part 2: $part2Value")
+}
+
+fun part1Assertions() {
     assert(findCommonItem("vJrwpWtwJgWrhcsFMMfFFhFp") == 'p')
     assert(findCommonItem("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == 'L')
     assert(findCommonItem("PmmdzqPrVvPwwTWBwg") == 'P')
     assert(findCommonItem("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") == 'v')
     assert(findCommonItem("ttgJtRGJQctTZtZT") == 't')
     assert(findCommonItem("CrZsJsPPZsGzwwsLwLmpwMDw") == 's')
+}
 
-    val part1Value = input.sumOf { findCommonItem(it).getPriority() }
-    println("Part 1: $part1Value")
+fun part2Assertions() {
+    assert(
+        getCommonItemAndPriority(
+            listOf(
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+            ),
+        ) == 'r' to 18,
+    )
+
+    assert(
+        getCommonItemAndPriority(
+            listOf(
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw",
+            ),
+        ) == 'Z' to 52,
+    )
 }
