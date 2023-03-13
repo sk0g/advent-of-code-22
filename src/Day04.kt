7f858ac0aa27841213100f4b1ab0a254a2f97eab
sk0g <ani.mehta@mail.com>
2023-03-13 21:45:02 +1000

    4-2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,4 +1,4 @@
-fun sectionsOverlap(s: String): Boolean {
+fun sectionsOverlapCompletely(s: String): Boolean {
     val (assignment1, assignment2) = s.split(",")
     val bounds1 = assignment1.toBounds()
     val bounds2 = assignment2.toBounds()
@@ -6,6 +6,13 @@ fun sectionsOverlap(s: String): Boolean {
     var overlaps = bounds1.first <= bounds2.first && bounds1.second >= bounds2.second
     overlaps = overlaps || bounds2.first <= bounds1.first && bounds2.second >= bounds1.second
     return overlaps
+}
+
+fun sectionsOverlapPartially(s: String): Boolean {
+    val (bounds1, bounds2) = s
+        .split(",")
+        .map { it.toBounds() }
+    return bounds1.first <= bounds2.second && bounds1.second >= bounds2.first
 }
 
 fun String.toBounds(): Pair<Int, Int> {
@@ -17,18 +24,31 @@ fun String.toBounds(): Pair<Int, Int> {
 
 fun main() {
     fun part1Assertions() {
-        assert(!sectionsOverlap("2-4,6-8"))
-        assert(!sectionsOverlap("2-3,4-5"))
-        assert(!sectionsOverlap("5-7,7-9"))
-        assert(sectionsOverlap("2-8,3-7"))
-        assert(sectionsOverlap("6-6,4-6"))
-        assert(!sectionsOverlap("2-6,4-8"))
+        assert(!sectionsOverlapCompletely("2-4,6-8"))
+        assert(!sectionsOverlapCompletely("2-3,4-5"))
+        assert(!sectionsOverlapCompletely("5-7,7-9"))
+        assert(sectionsOverlapCompletely("2-8,3-7"))
+        assert(sectionsOverlapCompletely("6-6,4-6"))
+        assert(!sectionsOverlapCompletely("2-6,4-8"))
+    }
+
+    fun part2Assertions() {
+        assert(!sectionsOverlapPartially("2-4,6-8"))
+        assert(!sectionsOverlapPartially("2-3,4-5"))
+        assert(sectionsOverlapPartially("5-7,7-9"))
+        assert(sectionsOverlapPartially("2-8,3-7"))
+        assert(sectionsOverlapPartially("6-6,4-6"))
+        assert(sectionsOverlapPartially("2-6,4-8"))
     }
 
     part1Assertions()
+    part2Assertions()
 
     val input = readInput("Day04")
 
-    val part1Answer = input.count { sectionsOverlap(it) }
+    val part1Answer = input.count { sectionsOverlapCompletely(it) }
     println("Part 1: $part1Answer")
+
+    val part2Answer = input.count { sectionsOverlapPartially(it) }
+    println("Part 2: $part2Answer")
 }
